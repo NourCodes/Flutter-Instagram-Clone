84584e8a1c4fbf7a449f12bdcaddbcd24dd4e896
NourCodes <nouralhamwi2018@gmail.com>
2024-02-22 21:30:51 +0300

    Displayed current user's username dynamically in the app interface

--- lib/responsive_layout/mobile_layout.dart
+++ lib/responsive_layout/mobile_layout.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class MobileScreen extends StatelessWidget {
+class MobileScreen extends StatefulWidget {
   const MobileScreen({Key? key}) : super(key: key);
 
   @override
+  State<MobileScreen> createState() => _MobileScreenState();
+}
+
+class _MobileScreenState extends State<MobileScreen> {
+  // variable to store the user's name
+  String name = '';
+
+  @override
+  void initState() {
+    // call getUsername() method when the screen initializes
+    getUsername();
+    super.initState();
+  }
+
+  // get the current user's ID
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+
+  // method to fetch the user's username from Firestore
+  void getUsername() async {
+    // get the document snapshot of the user with the current UID
+    DocumentSnapshot snap =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
+
+    // extract the username from the document snapshot
+    String username = (snap.data()! as Map<String, dynamic>)['username'];
+
+    // update the 'name' variable with the fetched username
+    setState(() {
+      name = username;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Mobile"),
+        child: Text(name),
       ),
     );
   }
